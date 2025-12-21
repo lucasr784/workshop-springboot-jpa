3116d304deaf84ce5cfb17380dd69ebdb2fe986b
lucasr784 <lucasrochapt784@gmail.com>
2025-12-21 20:49:49 +0000

    Order, Instant, ISO 8601 - PART 1

--- src/main/java/com/springproject/course/entities/User.java
+++ src/main/java/com/springproject/course/entities/User.java
@@ -1,13 +1,15 @@
 package com.springproject.course.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
-
 
 @Entity
 @Table(name = "tb_user")
@@ -21,6 +23,9 @@ public class User implements Serializable{
     private String email;   
     private String phone;
     private String password;
+
+    @OneToMany(mappedBy="client")
+    private List<Order> orders = new ArrayList<>();
 
     public User(){
     };
@@ -75,6 +80,10 @@ public class User implements Serializable{
         this.password = password;
     }
 
+    public List<Order> getOrders() {
+        return orders;
+    }
+
     @Override
     public int hashCode() {
         final int prime = 31;
@@ -99,6 +108,8 @@ public class User implements Serializable{
             return false;
         return true;
     }
+
+    
 
 
 
